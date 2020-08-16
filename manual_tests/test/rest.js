47c49089f513cecf573031baf08b5088b78efb8e
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-08-16 02:30:02 -0700

    fix tests

--- manual_tests/test/rest.js
+++ manual_tests/test/rest.js
@@ -3,33 +3,38 @@ const expect = require("chai").expect;
 
 describe("REST API", () => {
   it("PUT", async () => {
-    const {resp, body} = await put(42);
+    const {resp, body} = await execRaw("CREATE TABLE IF NOT EXISTS person (id INTEGER PRIMARY KEY, name TEXT NOT NULL)");
 
-    expect(resp.statusCode).to.eql(204);
-    expect(body).to.be.undefined;
+    expect(resp.statusCode).to.eql(200);
+    expect(body).to.be.null;
   });
 
   it("GET", async () => {
-    await put({ a: 42, b: "asdf" });
-    const {resp, body} = await get();
+    await execRaw("DELETE FROM person");
+    await execRaw("INSERT INTO person (name) VALUES ('alice'), ('bob'), ('carol')");
+    const {resp, body} = await queryRaw("SELECT id, name FROM person");
 
     expect(resp.statusCode).to.eql(200);
-    expect(body).to.eql({ a: 42, b: "asdf" });
+    expect(body).to.eql([
+      {id: 1, name: "alice"},
+      {id: 2, name: "bob"},
+      {id: 3, name: "carol"},
+    ]);
   });
 });
 
-async function get() {
-  return req({ uri: "http://localhost:9000/v0/", method: "GET", json: true });
+async function queryRaw(body) {
+  return req({ uri: "http://localhost:9000/v0/raw", method: "GET", body });
 }
 
-async function put(data) {
-  return req({ uri: "http://localhost:9000/v0/", method: "PUT", json: data });
+async function execRaw(body) {
+  return req({ uri: "http://localhost:9000/v0/raw", method: "POST", body });
 }
 
 async function req(opts) {
   return new Promise((resolve, reject) => {
     request(opts, (err, resp, body) => {
-      return err ? reject(err) : resolve({resp, body});
+      return err ? reject(err) : resolve({resp, body: JSON.parse(body)});
     });
   });
 }
